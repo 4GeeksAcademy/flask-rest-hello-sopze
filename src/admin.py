960880d04f088dcf6d856b4f5a273eaa2b57a260
Sergio Sopze <60981650+Sopze92@users.noreply.github.com>
2024-05-01 10:55:58 +0000

    added tools FE + made reactive + final styles

--- src/admin.py
+++ src/admin.py
@@ -4,8 +4,8 @@ from models import db, User, Entity, EntityType, Bookmark
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
-    app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    app.secret_key = os.environ.get('FLASK_APP_KEY', 'default_key')
+    app.config['FLASK_ADMIN_SWATCH'] = os.environ.get('BOOTSTRAP_THEME', 'slate')
     admin = Admin(app, template_mode='bootstrap3')
 
     admin.add_view(ModelView(User, db.session))
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 from flask import jsonify, url_for
-import re
+import os, re, tools
 
 class APIException(Exception):
     status_code = 400
@@ -23,55 +23,61 @@ def has_no_empty_params(rule):
 
 def _get_url_color(url):
     if "admin" in url:
-        return "#ffff00"
+        return "admin"
     if "api" in url:
-        return "#66ff00"
+        return "api"
     elif "db" in url:
-        return "#ff6600"
+        return "db"
     elif url == "/":
-        return "#A4A4A4"
-    return "#aa77ff"
+        return "root"
+    return "other"
 
 def _get_link(url):
     return [url, _get_url_color(url)]
 
 def _get_method(methods):
     if "DELETE" in methods:
-        return [3, "DELETE"]
+        return "DELETE"
     elif "POST" in methods:
-        return [2, "POST"]
+        return "POST"
     elif "PUT" in methods:
-        return [1, "PUT"]
-    return [0, "GET"]
+        return "PUT"
+    return "GET"
 
 def generate_sitemap(app):
     admin_raw_link= '/admin/'
     endpoint_raw_link= []
     endpoint_raw_span= []
+
+    method_get= [_get_method("GET")]
+
     for rule in app.url_map.iter_rules():
-        if "GET" in rule.methods and has_no_empty_params(rule) and not 'wipe' in rule.endpoint:
-            link= _get_method("GET") + _get_link(url_for(rule.endpoint, **(rule.defaults or {})))
-            if 'admin' not in link[2]:
-                endpoint_raw_link.append(link)
+        if "GET" in rule.methods and has_no_empty_params(rule) and not re.search(r'wipe|execute', rule.endpoint):
+            link= _get_link(url_for(rule.endpoint, **(rule.defaults or {})))
+            if 'admin' not in link[0]:
+                endpoint_raw_link.append(method_get + link)
         else:
             str= rule.__str__()
             if not re.search(r'static|export|admin', str):
-                result= _get_method(rule.methods) + [rule.__str__()]
-                if '<' in result[2]:
-                    result[2]= result[2].replace('<','<span class="arg">&lt;').replace('>','&gt;</span>')
-                endpoint_raw_span.append(result)
+                rulestr= rule.__str__()
+                if '<' in rulestr:
+                    rulestr= rulestr.replace('<','<span class="arg">&lt;').replace('>','&gt;</span>')
+                endpoint_raw_span.append([_get_method(rule.methods), rulestr])
 
     admin_link= f"<p><a class=\"admin\" href=\"{admin_raw_link}\">Site admin: <span>{admin_raw_link}</span></a></p>"
-    endpoint_link = "".join([f"<li><div class=\"ept{y[0]}\">{y[1]}</div><a style=\"color: {y[3]}\" href=\"{y[2]}\">{y[2]}</a></li>" for y in endpoint_raw_link])
-    endpoint_span = "".join([f"<li><div class=\"ept{y[0]}\">{y[1]}</div>{y[2]}</li>" for y in endpoint_raw_span])
+    endpoint_link = "".join([f"<li><div class=\"method method-{y[0].lower()}\">{y[0]}</div><a class=\"apilink-{y[2]}\" href=\"{y[1]}\">{y[1]}</a></li>" for y in endpoint_raw_link])
+    endpoint_span = "".join([f"<li><div class=\"method method-{y[0].lower()}\">{y[0]}</div>{y[1]}</li>" for y in endpoint_raw_span])
+    tool_list = "".join([f"<li>{t}</li>" for t in tools.generate_html_links()])
 
     html= open("res/index.html",'r').read()
     css= open("res/styles.css",'r').read()
 
     page= html.replace(
-        "{css}",css).replace(
-        "{admin_link}",admin_link).replace(
-        "{endpoint_link}",endpoint_link).replace(
-        "{endpoint_span}",endpoint_span)
+        "%SWATCH%", os.environ.get('BOOTSTRAP_THEME', 'slate')).replace(
+        "%CSS%",css).replace(
+        "%ADMIN_LINK%",admin_link).replace(
+        "%ENDPOINT_LINK%",endpoint_link).replace(
+        "%ENDPOINT_SPAN%",endpoint_span).replace(
+        "%TOOL_LIST%",tool_list)
 
     return page
