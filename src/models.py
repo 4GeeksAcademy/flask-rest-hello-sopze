87b51d96839e9129ce192cb9e7fb6429be96a633
Sergio Sopze <60981650+Sopze92@users.noreply.github.com>
2024-05-01 10:58:52 +0000

    final models + defaults

--- src/models.py
+++ src/models.py
@@ -3,17 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-PROPERTIES= {
-    "user": [""]
-}
-
 class User(db.Model):
     __tablename__ = "users"
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(32), unique=True, nullable=False)
-    email = db.Column(db.String(32), unique=True, nullable=False)
-    password = db.Column(db.String(32), nullable=False)
-    bookmarks = db.relationship('Bookmark', backref='user', lazy='dynamic')
+    username = db.Column(db.String(32), unique=True, nullable=False)            # public username
+    email = db.Column(db.String(32), unique=True, nullable=False)               # user email
+    password = db.Column(db.String(32), nullable=False)                         # account password
+    user_token= db.Column(db.String(256))                                       # current login session token, or null if not logged-in
+    bookmarks = db.relationship('Bookmark', backref='user', lazy='dynamic')     # bookmarks
 
     def __repr__(self):
         return f'<User {self._id}-{self.username}-{self.email}>'
@@ -28,11 +25,11 @@ class User(db.Model):
 class Entity(db.Model):
     __tablename__ = "entities"
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    _tid = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(64), unique=True, nullable=False)
-    description = db.Column(db.String(512))
-    properties = db.Column(db.String(32768), nullable=False)
-    type_id = db.Column(db.Integer, db.ForeignKey("entity_types._id"), nullable=False)
+    _tid = db.Column(db.Integer, nullable=False)                                        # per-type autoincremental id
+    name = db.Column(db.String(32), unique=True, nullable=False)                        # entity display name
+    description = db.Column(db.String(128))                                             # entity display description
+    properties = db.Column(db.String(2048), nullable=False)                             # json-formatted string with all the required properties
+    type_id = db.Column(db.Integer, db.ForeignKey("entity_types._id"), nullable=False)  # entitytype
 
     def get_tid_(self, type):
         return self.query.filter_by(type_id=type).count()+1
@@ -55,13 +52,15 @@ class Entity(db.Model):
             "name": self.name,
             "description": self.description,
             "type_id": self.type_id,
-            "properties": json.loads(self.properties),
+            #"properties_str": self.properties,
+            "properties": json.loads(self.properties)
         }
 
 class EntityType(db.Model):
     __tablename__ = "entity_types"
     _id= db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name= db.Column(db.String(32), unique=True, nullable=False)
+    name= db.Column(db.String(32), unique=True, nullable=False)         # type name
+    properties= db.Column(db.String(2048), nullable=False)              # required properties for its type
 
     def __repr__(self):
         return f'<EntityType {self._id}-{self.name}>'
@@ -69,14 +68,15 @@ class EntityType(db.Model):
     def serialize(self):
         return {
             "_id": self._id,
-            "name": self.name
+            "name": self.name,
+            "properties": self.properties.split('|')
         }
     
 class Bookmark(db.Model):
     __tablename__ = "bookmarks"
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users._id"), nullable=False)
-    entity_id = db.Column(db.Integer, db.ForeignKey("entities._id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users._id"), nullable=False)         # owner user id
+    entity_id = db.Column(db.Integer, db.ForeignKey("entities._id"), nullable=False)    # entity this bookmark refeers to
     entity = db.relationship('Entity')
 
     def __repr__(self):
